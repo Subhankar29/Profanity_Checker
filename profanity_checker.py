7ec58964433271ea34a5cbb68a11d36c160e6a90
subhankar29 <subhankar29shah@gmail.com>
2017-02-07 23:32:23 +0530

    Testing in a file

---
+++ profanity_checker.py
@@ -0,0 +1,10 @@
+def read_text():
+    quotes = open("/Users/Subhankar29/Desktop/email_checker/email_checker.txt")
+    contents = quotes.read()
+    print(contents)
+    quotes.close()
+
+
+    
+
+read_text()            
