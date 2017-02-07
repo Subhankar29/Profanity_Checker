17c06eba6c9bd9708ed66643c405d15fa4271be8
subhankar29 <subhankar29shah@gmail.com>
2017-02-08 00:06:59 +0530

    Checks the document for profanity

--- profanity_checker.py
+++ profanity_checker.py
@@ -1,10 +1,21 @@
+import urllib
 def read_text():
     quotes = open("/Users/Subhankar29/Desktop/email_checker/email_checker.txt")
     contents = quotes.read()
-    print(contents)
+    #print(contents)
     quotes.close()
+    check_profanity(contents)
 
-
-    
+def check_profanity(text_to_check):
+    connection=urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output = connection.read()
+    #print(output)
+    connection.close()
+    if "true" in output:
+        print("Profanity Alert!")
+    elif "false" in output:
+        print("This document has no curse word")
+    else:
+        print("Could not  scan the document properly.")
 
 read_text()            
